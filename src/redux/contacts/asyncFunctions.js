c30505914bc293f3e753315ad2efe3e315e37136
Danik2023 <danik.kyrteu2@gmail.com>
2025-03-10 17:14:26 +0200

    ...

--- src/redux/contacts/asyncFunctions.js
+++ src/redux/contacts/asyncFunctions.js
@@ -1,8 +1,6 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-// axios.defaults.baseURL = 'https://connections-api.goit.global/';
-
 export const fetchContacts = createAsyncThunk('contacts/fetchAll', async () => {
   const response = await axios.get('/contacts');
   return response.data;
