86e1136cfe588618ced993bcbf29241ee6240e1c
Danik2023 <danik.kyrteu2@gmail.com>
2025-03-11 20:32:16 +0200

    ...

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,3 @@
-import { ContactForm } from './ContactForm/ContactForm';
-import { Filter } from './Filter/Filter';
-import { ContactListComponent } from './ContactList/ContactList';
-
 export const App = () => {
-  return (
-    <div style={{ marginBottom: '50px' }}>
-      <ContactForm />
-      <Filter />
-      <ContactListComponent />
-    </div>
-  );
+  return <div></div>;
 };
