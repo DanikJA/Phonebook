81c11e7968f9f7d6cfe7f24acfad481fbb881ed0
Danik2023 <danik.kyrteu2@gmail.com>
2025-03-13 21:07:53 +0200

    ...

--- src/components/AppBar/AppBar.js
+++ src/components/AppBar/AppBar.js
@@ -2,13 +2,14 @@ import { Navigation } from 'components/Navigation/Navigation';
 import { useAuth } from '../../redux/hooks/auth';
 import { UserMenu } from 'components/UserMenu/UserMenu';
 import { AuthNav } from 'components/AuthNav/AuthNav';
+import { Header } from './AppBarStyled';
 
 export const AppBar = () => {
   const { isLoggedIn } = useAuth();
   return (
-    <header>
+    <Header>
       <Navigation />
       {isLoggedIn ? <UserMenu /> : <AuthNav />}
-    </header>
+    </Header>
   );
 };
--- src/components/AppBar/AppBarStyled.js
+++ src/components/AppBar/AppBarStyled.js
@@ -1,1 +1,10 @@
 import styled from 'styled-components';
+
+export const Header = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 15px 30px;
+  background-color: #282c34;
+  color: white;
+`;
