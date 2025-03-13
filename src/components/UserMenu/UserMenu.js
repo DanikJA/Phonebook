2b00352563ed5f868c316ca586758b965f45eb16
Danik2023 <danik.kyrteu2@gmail.com>
2025-03-13 18:40:19 +0200

    ...

--- src/components/UserMenu/UserMenu.js
+++ src/components/UserMenu/UserMenu.js
@@ -1,5 +1,6 @@
 import { useDispatch } from '@reduxjs/toolkit';
 import { useAuth } from '../../redux/hooks/auth';
+import { logOut } from '../../redux/auth/asyncFunctions';
 
 export const UserMenu = () => {
   const dispatch = useDispatch();
@@ -7,8 +8,8 @@ export const UserMenu = () => {
 
   return (
     <div>
-      <p>Welcome,{user.name}</p>
-      <button type="button" onClick={() => dispatch()}></button>
+      <p>{user.email}</p>
+      <button type="button" onClick={() => dispatch(logOut)}></button>
     </div>
   );
 };
