b93d2628661732d32195c0ea0e050647d257513e
Danik2023 <danik.kyrteu2@gmail.com>
2025-03-14 14:43:29 +0200

    ...

--- src/components/Pages/HomePage/HomePage.js
+++ src/components/Pages/HomePage/HomePage.js
@@ -4,7 +4,7 @@ import {
   HomePageContainer,
   StyledNavLink,
   SubTitle,
-} from './HomeStyled';
+} from './HomePageStyled';
 
 export const HomePage = () => {
   const { isLoggedIn } = useAuth();
