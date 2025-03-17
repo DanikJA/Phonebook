37c987a224cd69f48639073985972522c200ddcc
Danik2023 <danik.kyrteu2@gmail.com>
2025-03-17 18:53:39 +0200

    ...

--- src/redux/contacts/selectors.js
+++ src/redux/contacts/selectors.js
@@ -1,3 +1,3 @@
-export const selectContacts = state => state.contacts.items;
-export const selectFilter = state => state.filter;
-export const isLoading = state => state.contacts.isLoading;
+export const selectContacts = state => state.contacts.contacts.items;
+export const selectFilter = state => state.contacts.filter;
+export const isLoading = state => state.contacts.contacts.isLoading;
